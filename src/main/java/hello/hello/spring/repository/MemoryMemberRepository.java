1773ebb962cb834a66c289b8c326891b54a93e8d
seojun <seojun1208@gmail.com>
2021-10-04 12:06:38 +0900

    회원 도메인, 리포지토리

---
+++ src/main/java/hello/hello/spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hello.spring.repository;
+
+import hello.hello.spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) { //이름
+        return store.values().stream() //루프로 돌린다
+                .filter(member -> member.getName().equals(name)) //member.setName()이 파라미터로 넘어온 name과 같은지 확인
+                .findAny(); //이름을 하나라도 찾으면 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); //store의 member들을 반환
+    }
+}
